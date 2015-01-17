fb5fd736937e19d6ea9bf994132e94afdf597462
Maximilian Hristache <mhristache@gmail.com>
2015-01-17 23:17:32 +0100

    change output format and add tests

--- ip_converter/src/main.rs
+++ ip_converter/src/main.rs
@@ -1,5 +1,5 @@
 use std::os;
-use std::num::{Int, from_str_radix}; //Needed so that pow() works
+use std::num::{Int, from_str_radix, cast}; 
 use std::io::net::ip::IpAddr;
 
 fn ipv4_to_int(ip: [u8; 4]) -> u32 {
@@ -15,7 +15,13 @@ fn int_to_ipv4(i: u32) -> IpAddr {
     IpAddr::Ipv4Addr(ip[0], ip[1], ip[2], ip[3])    
 }
 
+struct Output {
+    ipv4: IpAddr,
+    int32: u32,
+    }
+
 fn main() {
+    let mut output: Output = Output{ipv4: IpAddr::Ipv4Addr(0,0,0,0), int32: 0};
     let s = os::args(); 
     if !(s.len() > 1) {
         println!("Usage: ./{} IP_ADDRESS", s[0]);
@@ -24,9 +30,8 @@ fn main() {
     let input: Option<IpAddr> = (s[1].as_slice().trim()).parse();
     match input {
         Some(IpAddr::Ipv4Addr(a, b, c, d)) => {
-            let int_ip = ipv4_to_int([a, b, c, d]);
-            println!("IP in uint32 format: {} (0x{:08X})", int_ip, int_ip);
-            return;       
+            output.ipv4 = input.unwrap();
+            output.int32 = ipv4_to_int([a, b, c, d]);       
         },
         Some(IpAddr::Ipv6Addr(..)) => {
             println!("Conversion not supported for IPv6 addresses!");
@@ -36,22 +41,43 @@ fn main() {
             let input: Option<u32> = (s[1].as_slice().trim()).parse();
             match input {
                 Some(ip_int) => {
-                    println!("IP in dec format: {}", int_to_ipv4(ip_int));
-                    return;
+                    output.ipv4 = int_to_ipv4(ip_int);
+                    output.int32 = input.unwrap();
                 },
                 None => {
                     let input =  s[1].as_slice().trim();
                     if input.starts_with("0x") {
                         let input = input.slice_from(2);
                         if let Some(x) = from_str_radix(input, 16) {
-                            println!("IP in dec format: {}", int_to_ipv4(x));
+                            output.ipv4 = int_to_ipv4(x);
+                            output.int32 = x;
+                        } else {
+                            println!("Invalid value: 0x{}", input);
                             return;
-                        }
+                            }
+                    } else {
+                        println!("Unrecognized IP format!");
+                        return;
                     }
-                    println!("Unrecognized IP format!");
-                    return;
                 }
             }
         }
     }
+    println!("{}, 0x{:08X}, {}", output.ipv4, output.int32, output.int32)
 }
+
+
+#[test]
+fn test_from_ipv4() {
+    assert!(3232236035u32 == ipv4_to_int([192, 168, 2, 3]));
+}
+
+#[test]
+fn test_from_int() {
+     match int_to_ipv4(3232236035u32) {
+        IpAddr::Ipv4Addr(a, b, c, d) => assert!([a, b, c, d] == [192, 168,2, 3]),
+        _ => assert!(1 == 0)
+     }
+}
+
+
