d21fe8fac8ce78b33c1f6818d6ec59d14836ab42
Maximilian Hristache <mhristache@gmail.com>
2015-03-22 18:00:15 +0100

    update to latest rust std::net changes

--- ip_converter/src/main.rs
+++ ip_converter/src/main.rs
@@ -1,10 +1,9 @@
-use std::os;
 use std::num::{Int, from_str_radix}; 
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 fn ipv4_to_int(ip: Ipv4Addr) -> u32 {
     let ip_oct = ip.octets();
-    (0..4).fold(0, |sum, x| {sum + (256.pow(ip_oct.len() - x - 1) * (ip_oct[x] as u32))})
+    (0..4).fold(0, |sum, x| {sum + (256.pow((ip_oct.len() - x - 1) as u32) * (ip_oct[x] as u32))})
 }
 
 fn int_to_ipv4(i: u32) -> Ipv4Addr {
@@ -28,41 +27,37 @@ fn main() {
     let s: Vec<_> = std::env::args().take(2).collect(); 
     if s.len() == 2 {
         let input = s[1].clone();
-        match input.trim().parse() {
-            Ok(IpAddr::V4(ip)) => {
-                output.ipv4 = ip;
-                output.int32 = ipv4_to_int(ip);       
-            },
-            Ok(IpAddr::V6(_)) => {
-                println!("Conversion not supported for IPv6 addresses!");
-                return;
-            },
-            Err(_) => {
-                match input.trim().parse() {
-                    Ok(ip_int) => {
-                        output.ipv4 = int_to_ipv4(ip_int);
-                        output.int32 = ip_int;
-                    },
-                    Err(_) => {
-                        if input.trim().starts_with("0x") {
-                            if let Ok(x) = from_str_radix(&input[2..], 16) {
-                                output.ipv4 = int_to_ipv4(x);
-                                output.int32 = x;
-                            } else {
-                                println!("Invalid value: 0x{}", input);
-                                return;
-                            }
+        if let Ok(ip) = input.trim().parse::<Ipv4Addr>() {
+            output.ipv4 = ip;
+            output.int32 = ipv4_to_int(ip);       
+        } else if let Ok(_) = input.trim().parse::<Ipv6Addr>() {
+            println!("Conversion not supported for IPv6 addresses!");
+            return;
+        } else {
+            match input.trim().parse() {
+                Ok(ip_int) => {
+                    output.ipv4 = int_to_ipv4(ip_int);
+                    output.int32 = ip_int;
+                },
+                Err(_) => {
+                    if input.trim().starts_with("0x") {
+                        if let Ok(x) = from_str_radix(&input[2..], 16) {
+                            output.ipv4 = int_to_ipv4(x);
+                            output.int32 = x;
                         } else {
-                            println!("Unrecognized IP format!");
+                            println!("Invalid value: 0x{}", input);
                             return;
                         }
+                    } else {
+                        println!("Unrecognized IP format!");
+                        return;
                     }
                 }
             }
         }
         println!("{}, 0x{:08X}, {}", output.ipv4, output.int32, output.int32);
     } else {
-        println!("Usage: ./{} IP_ADDRESS", s[0]);
+        println!("Usage: {} IP_ADDRESS", s[0]);
         return;
    }     
 }
